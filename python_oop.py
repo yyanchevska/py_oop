1e4ffee8bd431f4bd16e83afdf9447a260e6a787
rhonyshniuk <rr.gonishnuk@gmail.com>
2020-07-20 09:54:48 +0300

    Add homework

---
+++ python_oop.py
@@ -0,0 +1,22 @@
+"""
+Description: Create TestRail client for generating Test Suites
+
+Features:
+- TestSuite should be created with (username list(First Name/Last Name), user_email, user_password)
+- Show in test cases all user data except password (password should be shown as '********')
+- Create test suite for login/registration/article create flow
+- Please use the same steps in common methods
+- Validate valid user email and profile
+
+Testing:
+- Run all your testsuite using calling methods for suite_object
+"""
+
+
+class TestRunBuilder:
+    pass
+
+# test_run_smoke = TestRunBuilder(username, user_email, user_password)
+# test_run_sanity = TestRunBuilder(username, user_email, user_password)
+# test_run_regression = TestRunBuilder(username, user_email, user_password)
+
